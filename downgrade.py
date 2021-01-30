056895ec81c977e8024e93c6563b7d1bdd002ac4
d0tus3r <d0tus3r@gmail.com>
2021-01-29 23:28:59 -0500

    turns out upgrade was an alias

--- downgrade.py
+++ downgrade.py
@@ -2,7 +2,7 @@
 '''Run script with 2 params:
 @password = ssh password found in site settings
 @ip = ip address of uap'''
-from paramiko import SSHClient
+import paramiko
 import sys
 
 #python3 downgrade.py 192.168.1.2 askjdnkjeu23jaw
@@ -10,11 +10,14 @@ import sys
 USERNAME = "admin"
 PASSWORD = sys.argv[2]
 IP = sys.argv[1]
-client = SSHClient()
-COMMAND = "upgrade https://dl.ui.com/unifi/firmware/U7PG2/4.3.20.11298/BZ.qca956x.v4.3.20.11298.200704.1347.bin"
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+COMMAND = "mca-cli-op upgrade https://dl.ui.com/unifi/firmware/U7PG2/4.3.20.11298/BZ.qca956x.v4.3.20.11298.200704.1347.bin"
 
 client.connect(IP, username=USERNAME, password=PASSWORD)
 stdin, stdout, stderr = client.exec_command(COMMAND)
 reply = stdout.readlines()
+reply2 = stdout.readlines()
 print(reply)
+print(reply2)
 client.close()
