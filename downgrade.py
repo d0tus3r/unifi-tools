0cdd1bd7e66039fc0e56c80b2a9196e358bd26db
Andrew <d0tus3r@gmail.com>
2021-04-15 23:13:48 -0400

    implemented reading ips from iplist.txt
    new usage is python3 downgrade.py sshpassword | provide your own iplist.txt file - ips separated by new line

--- downgrade.py
+++ downgrade.py
@@ -4,20 +4,27 @@
 @ip = ip address of uap'''
 import paramiko
 import sys
+import time
 
 #python3 downgrade.py 192.168.1.2 askjdnkjeu23jaw
 
 USERNAME = "admin"
-PASSWORD = sys.argv[2]
-IP = sys.argv[1]
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-COMMAND = "mca-cli-op upgrade https://dl.ui.com/unifi/firmware/U7PG2/4.3.20.11298/BZ.qca956x.v4.3.20.11298.200704.1347.bin"
+PASSWORD = sys.argv[1]
+#IP = sys.argv[1]
 
-client.connect(IP, username=USERNAME, password=PASSWORD)
-stdin, stdout, stderr = client.exec_command(COMMAND)
-reply = stdout.readlines()
-reply2 = stdout.readlines()
-print(reply)
-print(reply2)
-client.close()
+iplist = [ x.strip() for x in open ('iplist.txt').read().split('\n') if x ]
+
+for ip in iplist:
+	print("Connecting to " + ip + "...")
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+	COMMAND = "mca-cli-op upgrade https://dl.ui.com/unifi/firmware/U7PG2/4.3.20.11298/BZ.qca956x.v4.3.20.11298.200704.1347.bin"
+	client.connect(ip, username=USERNAME, password=PASSWORD)
+	stdin, stdout, stderr = client.exec_command(COMMAND)
+	reply = stdout.readlines()
+	reply2 = stdout.readlines()
+	print(reply)
+	print(reply2)
+	client.close()
+	print("Waiting for connection to close - may take up to 1 minute")
+	time.sleep(60)
